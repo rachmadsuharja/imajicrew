ede3ddae90025162100e4bd6afee534226f5a6ea
Rachmad Suharja <rachmadsuharja123@gmail.com>
2026-07-05 09:28:42 +0800

    feat: add dark mode theme

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.101.235"],
+};
 
 export default nextConfig;
